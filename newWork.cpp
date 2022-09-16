28995c85a5a3004db13ede06c25a807fbab8fbfd
Huwng <92002729+hungsvvl@users.noreply.github.com>
2022-09-16 05:39:32 -0700

    Update newWork.cpp
    code cơ bản

--- newWork.cpp
+++ newWork.cpp
@@ -1,1 +1,24 @@
-
+#include <iostream>
+#include <string>
+using namespasce std;
+struct NhanVien{
+	string tenNV;
+	string TgVoCa;
+	string TgRoiCa;
+	string TongTien;		
+	
+};
+NhanVien NhapNV(){
+	NhanVien x;
+	cout <<"Nhap vao ten nhan vien : ";
+	getline(cin, x.tenNV);		
+}
+NhanVien CaLam(){
+	TgVoCa	x;
+	cout <<"Nhap thoi gian vo ca lam : ";
+	getline(cin, x.TgVoCa);
+	return x;
+}
+int main (){
+	
+}
