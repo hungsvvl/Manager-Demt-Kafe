d244cef16af950221e0efe711816ce0a4037337a
Huwng <92002729+hungsvvl@users.noreply.github.com>
2022-10-12 16:55:55 +0700

    convert demo to offcial
    all code will add here

--- demo code.cpp
+++ demo code.cpp
@@ -1,116 +1,155 @@
-// sinh vien: ten, tuoi, diem ktlt
-
-// hanh dong: nhap, xuat, so sanh diem 2 sinh vien
-
-#include<iostream>
-
+#include <iostream>
+#include <string>
 using namespace std;
+class NhanVien {
 
-class SinhVien{
+protected:
+    string HoTen;
+    int NamVaoLam;
+public:
+    NhanVien() {
+        HoTen = "";
+        NamVaoLam = 0;
 
-	private:
-
-		char Ten[10];
-
-		int Tuoi;
-
-		float Diem;
-
-	public:
-
-		float getDiem();
-
-		void nhap();
-
-		void xuat();
-
-		void SoSanh1(SinhVien sv1);
+    }
+    virtual void NhapTT();
+    virtual void XuatTT();
 
 };
+void NhanVien::NhapTT() {
+    cout << "ho ten:";
+    getline(cin, HoTen);
+    cout << "nam vao lam :";
+    cin >> NamVaoLam;
 
-float SinhVien::getDiem(){
-
-	return Diem;
-
-}
-
-void SinhVien::nhap(){
-
-	cout<<"Nhap ten: ";
-
-	cin>>Ten;
-
-	cout<<"Nhap tuoi: ";
-
-	cin>>Tuoi;
-
-	cout<<"Nhap diem: ";
-
-	cin>>Diem;
 
 }
-
-void SinhVien::xuat(){
-
-	cout<<"Ten: "<<Ten<<endl;
-
-	cout<<"Tuoi: "<<Tuoi<<endl;
-
-	cout<<"Diem: "<<Diem<<endl;
+void NhanVien::XuatTT() {
+    cout << "Ho ten :" << HoTen << endl;
 
 }
+class NhanVienSanXuat : public NhanVien {
+private:
+    int Sosanpham;
+public:
+    NhanVienSanXuat() {
+        Sosanpham = 0;
 
-// Thiet ke ham so sanh la ham ngoai lop
-
-void SoSanh(SinhVien sv1, SinhVien sv2){
-
-	if(sv1.getDiem() == sv2.getDiem())
-
-		cout<<"Bang nhau";
-
-	else if(sv1.getDiem() > sv2.getDiem())
-
-		cout<<"Lon hon";
-
-	else
-
-		cout<<"Be hon";
-
-}
-
-// Thiet ke ham so sanh la phuong thuc cua lop
-
-void SinhVien::SoSanh1(SinhVien sv1){
-
-	if(Diem == sv1.Diem)
-
-		cout<<"Bang nhau";
-
-	else if(Diem > sv1.Diem)
-
-		cout<<"Lon hon";
-
-	else
-
-		cout<<"Be hon";
+    }
+    void NhapTT();
+    void XuatTT();
+    int TinhLuong=0;
+};
+void NhanVienSanXuat::NhapTT() {
+    NhanVien::NhapTT();
+    cout << "so san pham";
+    cout << "luong" << TinhLuong();
 
 }
+int NhanVienSanXuat::TinhLuong() {
+    int Luong = 0;
+    Luong = (Sosanpham * 10000 ) + (100000 + ((2016 - NamVaoLam) * 2000));
+    return Luong;
+
+}
+class NhanVienVanPhong : public NhanVien{
+    private:
+        int MucLuong, SoNgayNghi;
+        public:
+            NhanVienVanPhong() {
+                MucLuong = 0;
+
+            }
+            void NhapTT();
+            void XuatTT();
+            int TinhLuong;
+};
+void NhanVienVanPhong::NhapTT() {
+    NhanVien::NhapTT();
+    cout << "Muc luong";
+    cin >> MucLuong;
+    cout << "so ngay nghi";
+    cin >> SoNgayNghi;
+    
+}
+void NhanVienVanPhong::XuatTT() {
+    NhanVien::XuatTT();
+    cout << "muc luong" << MucLuong << endl;
+    cout << "so ngay nghi" << SoNgayNghi << endl;
+    cout<<"luong"<<TinhLuong();
+}
+int NhanVienVanPhong::TinhLuong() {
+    int Luong = 0;
+    Luong = MucLuong - SoNgayNghi * 10000 + 100000 + ((2016 - NamVaoLam) * 20000);
+    return Luong;
+}
+class QuanLyNhanVien {
+private:
+    vector<NhanVien*> dsNhanVien;
+    void NhapDanhSach();
+    void XuatDanhSach();
+    
+
+};
+void QuanLyNhanVien::NhapDanhSach() {
+    int chon;
+    NhanVien* nv;
+    do{
+        system("cls");
+        cout << "1.Nhan vien san xuat" << endl;
+        cout << "2. Nhan vien van phong" << endl;
+        cout << "0. Thoat" << endl;
+        cin >> chon;
+        switch (chon) {
+        case 1:
+        {
+            nv = new NhanVienSanXuat();
+            nv->NhapTT();
+            dsNhanVien.push_back(nv);
+            break;
+
+        }
+        case 2: 
+        {
+            nv = new NhanVienVanPhong();
+            nv->NhapTT();
+            dsNhanVien.push_back(nv);
+            break;
+        }
+        case 3:
+        {
+            break;
+        }
+    }
+        }while (chon != 0);
+
+}
+void QuanLyNhanVien::XuatDanhSach(){
+    for (int i = 0; i < dsNhanVien.size(); i++) {
+        dsNhanVien[i]->XuatTT();
+        cout << "========================" << endl;
+    }
 
 int main(){
-
-	SinhVien sv1, sv2;
-
-	sv1.nhap();
-
-	sv2.nhap();
-
-	cout<<"\nDiem cua sv1 ";
-
-	//SoSanh(sv1,sv2);
-
-	sv1.SoSanh1(sv2);
-
-	cout<<" diem cua sv2";
-
-}
-
+int chon;
+do
+{
+	system("cls");
+	cout<<"1. nhap nv"<<endl;
+	cout<<"2. xuat nv"<<endl;
+	cout<<"moi chon";
+	cin>>chon;
+	switch(chon){
+	case 1:
+		{
+			QLNV.NhapDanhSach();
+			break;
+		}
+		case 2:
+			{
+				QLNV.XuatDanhSach();
+				system("pause");
+				break;
+			}
+	}while(chon !=0);
+}}
